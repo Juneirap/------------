b809a4a764ecdc5326c7969b55f9c5303567ae5d
Juneirap <Juneircvb@gmail.com>
2024-03-09 16:37:04 +0700

    update

--- LAB3/html/listItem.php
+++ LAB3/html/listItem.php
@@ -1,13 +1,16 @@
 <?php
 include "condb.php";
 
-$sql = "SELECT * FROM tb_member ORDER BY member_name ASC"
-$result = mysqli_query($conn,$sql);
+$sql = "SELECT * FROM tb_member ORDER BY member_name ASC";
+$result = mysqli_query($conn, $sql);
 
-var_dump($result);
-
-
+// var_dump($result);
 ?>
+
+<!-- Button trigger modal -->
+<button id="btn_add" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    + ADD
+</button>
 
 <table>
     <thead>
@@ -20,13 +23,13 @@ var_dump($result);
     </thead>
     <tbody>
         <?php
-while($row = mysqli_fetch_assoc($result)) {
+        while ($row = mysqli_fetch_assoc($result)) {
         ?>
             <tr>
-                <td><?= $row['id_member']?></td>
-                <td><?= $row['member_name']?></td>
-                <td><?= $row['member_email']?></td>
-                <td><button class="btn_id" data-id="<?= $row ['id_member']?> ">del </button> </td>
+                <td><?= $row['id_member'] ?></td>
+                <td><?= $row['member_name'] ?></td>
+                <td><?= $row['member_email'] ?></td>
+                <td><button class="btn_id" data-id="<?= $row['id_member'] ?>">del</button></td>
             </tr>
         <?php
         }
@@ -35,27 +38,34 @@ while($row = mysqli_fetch_assoc($result)) {
 </table>
 
 <script>
-    $(function(){
-        $(".btn_id").click(function(){
+    $(function() {
+        $(".btn_id").click(function() {
             let id = $(this).data('id');
             console.log(id);
 
             $.ajax({
-                url:"/delitem.php",
-                method:"GET",
-                data:{
-                    mem_id:id
-                }
-                success:function(res) {
-                    if(res == 'error')
-                        alert("Can't delete item.")
+                url: "/delitem.php",
+                method: "GET",
+                data: {
+                    mem_id: id
+                },
+                success: function(res) {
+                    if (res == 'error')
+                        alert("Can't delete item.");
                     else
-                        $("#tb_member").load("/listItem.php")
+                        $("#tb_member").load("/listItem.php");
                 }
             });
         });
 
+        $("#btn_add").click(function() {
+            $("#staticBackdropLabel").text("Add member");
+            $(".modal-body").load("/add_form.php");
+            $(".modal-footer .btn-secondary").text("Cancle");
+            $(".modal-footer .btn-primary").text("Save");
 
-    });
+        });
 
+
+    }); //jQuery Ready
 </script>
